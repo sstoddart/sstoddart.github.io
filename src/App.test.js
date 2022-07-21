52436608c04fef183c8391a4072ba7b47be3fdca
sstoddart-vgw <samuel.stoddart@vgw.co>
2022-07-21 15:38:44 +0800

    Add link and test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/join us/i);
   expect(linkElement).toBeInTheDocument();
 });
